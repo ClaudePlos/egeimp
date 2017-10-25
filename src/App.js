bead634e01bc5e26170020f3ec07d50abf793ae8
ClaudePlos <claude-plos@o2.pl>
2017-10-25 16:34:46 +0200

    configure login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 import './App.css';
-
+import Loginscreen from './Loginscreen'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state={
+            loginPage:[],
+            uploadScreen:[]
+        }
+    }
+    componentWillMount(){
+        var loginPage =[];
+        loginPage.push(<Loginscreen parentContext={this} key="1"/>);
+        this.setState({
+            loginPage:loginPage
+        })
+    }
+    render() {
+        return (
+            <div className="App">
+                {this.state.loginPage}
+                {this.state.uploadScreen}
+            </div>
+        );
+    }
 }
-
+const style = {
+    margin: 15,
+};
 export default App;
